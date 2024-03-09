4aa54cd854dbd475c2c5205bf02cff20daf4e406
Kom46 <kommunizm46@gmail.com>
2024-03-09 22:32:43 +0300

    openteleChatBot implementation started

--- tg_chat_bot/chatBot.py
+++ tg_chat_bot/chatBot.py
@@ -1,3 +1,4 @@
+import os
 from aiogram import Bot, Router, filters
 from opentele.tl import TelegramClient
 from opentele.td import TDesktop
@@ -20,19 +21,18 @@ class aiogramChatBot:
         return self.__bot
 
     @property
-    def router(self):
-        return self.__router
-
-    @property
     def settings(self):
         return self.__settings
 
     def __init__(
         self,
-        settings: str,
+        settings: str | None = None,
+        api_key: str | None = None,
         router_name: str | None = None,
         *routers: Router,
     ) -> None:
+        if (settings is None) and (api_key is None):
+            raise Exception("Nor settings nor api_key is provided")
         parmsFileDes = open(settings, "r")
         params = js.load(parmsFileDes)
         api_token = params["api_key"]
@@ -45,12 +45,14 @@ class aiogramChatBot:
     def init_commands(self, callbacks: Dict[str, any]) -> None:
         self.__commands = callbacks
         for key, val in self.__commands.items():
-            self.router.message.register(val, filters.Command(key))
+            self.__router.message.register(val, filters.Command(key))
 
 
 class openteleChatBot(TelegramClient):
-    __desktop: TDesktop
     __tdata: str
+    __session: str
+    __client: TelegramClient
+    
 
     @property
     def desktop(self):
@@ -63,8 +65,8 @@ class openteleChatBot(TelegramClient):
     async def __init__(self, tdata: str = "") -> None:
         if self.test_tdata(tdata):
             self.__tdata = tdata
-            self.__desktop = TDesktop(tdata)
-            self.__client = await self.__desktop.ToTelethon(session="telethon.session", flag=UseCurrentSession)
+            desktop = TDesktop(tdata)
+            self.__client = await desktop.ToTelethon(session="telethon.session", flag=UseCurrentSession)
         else:
             raise Exception("tdata is not valid")
    
@@ -73,7 +75,7 @@ class openteleChatBot(TelegramClient):
         for key, val in callbacks.items():
             self.add_event_handler(val, events.NewMessage(pattern=key))
             
-    async def test_tdata(self, tdata: str) -> bool:
+    async def test_tdata(cls, tdata: str) -> bool:
         desktop = TDesktop(tdata)
         # if it is loaded and accounts more then 0 then try to connect
         result = desktop.isLoaded() and (desktop.accountsCount > 0)
@@ -86,4 +88,33 @@ class openteleChatBot(TelegramClient):
                 result = False
         return result
     
+    async def convert_tdata(cls, tdata: str, session_path: str = "telethon.session", flag: int = UseCurrentSession) -> str:
+        result = ""
+        if cls.test_tdata(tdata):
+            desktop = TDesktop(tdata)
+            desktop.ToTelethon(session=session_path, flag=flag)
+            if os.path.exists(session_path):
+                result = session_path
+        return result
     
+    async def get_proxy_from_file(self, filename: str) -> bool:
+        pass
+    
+    async def get_proxy_from_database(self, database: str) -> bool:
+        pass
+    
+    async def test_proxy(ip: str, port: int) -> bool:
+        pass
+    
+    def add_event_handler(self, callback: any, event: events.NewMessage, trigger: str = "") -> None:
+        result = True
+        if trigger != "":
+            event = events.NewMessage(pattern=trigger)
+        self.__client.add_event_handler(callback, event)
+        return result
+    
+    def start(self) -> None:
+        self.__client.run_until_disconnected()
+    
+    def stop(self) -> None:
+        self.__client.disconnect()
