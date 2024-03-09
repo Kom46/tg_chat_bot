1e3b69814240a071f01ad55b3d45247b5da8f039
Kom46 <kommunizm46@gmail.com>
2024-03-09 23:08:32 +0300

    Implementation of openteleChatBot in progress

--- tg_chat_bot/chatBot.py
+++ tg_chat_bot/chatBot.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 from aiogram import Bot, Router, filters
 from opentele.tl import TelegramClient
@@ -8,6 +9,24 @@ from telethon import events
 import json as js
 from typing import Dict
 
+
+class ProxyType(enumerate.Enum):
+    HTTP = "HTTP"
+    HTTPS = "HTTPS"
+    SOCKS4 = "SOCKS4"
+    SOCKS5 = "SOCKS5"
+    
+@dataclass
+class Proxy:
+    ip: str
+    port: int
+    type: ProxyType
+    
+@dataclass
+class User:
+    id: int
+    username: str
+    name: str
 
 class aiogramChatBot:
     __settings: str
@@ -52,7 +71,7 @@ class openteleChatBot(TelegramClient):
     __tdata: str
     __session: str
     __client: TelegramClient
-    
+    proxy: Proxy
 
     @property
     def desktop(self):
@@ -67,6 +86,7 @@ class openteleChatBot(TelegramClient):
             self.__tdata = tdata
             desktop = TDesktop(tdata)
             self.__client = await desktop.ToTelethon(session="telethon.session", flag=UseCurrentSession)
+            self.__client.start()
         else:
             raise Exception("tdata is not valid")
    
@@ -83,6 +103,7 @@ class openteleChatBot(TelegramClient):
             client = await desktop.ToTelethon(session="telethon.session", flag=CreateNewSession)
             try:
                 client.connect()
+                client.disconnect()
             except OSError:
                 # if we cannot connect to telegram - we cannot say what tdata is ok
                 result = False
@@ -97,24 +118,51 @@ class openteleChatBot(TelegramClient):
                 result = session_path
         return result
     
+    # TODO: #1 implement openteleChatBot.get_proxy_from_file
     async def get_proxy_from_file(self, filename: str) -> bool:
         pass
-    
+    # TODO: #2 implement openteleChatBot.get_proxy_from_database
     async def get_proxy_from_database(self, database: str) -> bool:
         pass
-    
+    # TODO: #3 implement openteleChatBot.test_proxy
     async def test_proxy(ip: str, port: int) -> bool:
         pass
     
-    def add_event_handler(self, callback: any, event: events.NewMessage, trigger: str = "") -> None:
+    def add_handler(self, callback: any, event: events.NewMessage, trigger: str = "") -> bool:
         result = True
         if trigger != "":
             event = events.NewMessage(pattern=trigger)
         self.__client.add_event_handler(callback, event)
         return result
     
+    def remove_handler(self, callback: any, event: events.NewMessage, trigger: str = "") -> bool:
+        result = True
+        if trigger != "":
+            event = events.NewMessage(pattern=trigger)
+        self.__client.remove_event_handler(callback, event)
+        return result
+    # TODO: #4 implement openteleChatBot.subscribe_channel
+    def subscribe_channel(channel: str | int) -> bool:
+        pass
+    # TODO: #5 implement openteleChatBot.unsubscribe_channel
+    def unsubscribe_channel(channel: str | int) -> bool:
+        pass
+    # TODO: #6 implement openteleChatBot.get_all_channel_users
+    def get_all_channel_users(channel: str | int) -> list[User]:
+        pass
+    # TODO: #7 implement openteleChatBot.filter_users
+    def filter_users(users: list[User], **filters) -> list[User]:
+        pass
+    # TODO: #8 implement openteleChatBot.export_users_to_excel
+    def export_users_to_excel(users: list[User], filename: str) -> bool:
+        pass
+    # TODO: #9 implement openteleChatBot.send_message
+    def send_message(self, user: User, message: str) -> bool:
+        pass
+    # TODO: #10 implement openteleChatBot.execute_mailing_list
+    def execute_mailing_list(self, users: list[User], message: str) -> bool:
+        pass
     def start(self) -> None:
         self.__client.run_until_disconnected()
-    
     def stop(self) -> None:
         self.__client.disconnect()
